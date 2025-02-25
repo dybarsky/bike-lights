21f418cf197852ca026004b9cd86b7f785e07278
Maksym Dybarskyi <maxim.dybarskyy@gmail.com>
2025-02-25 20:48:00 +0100

    Add pattern files

---
+++ firmware/src/headers/pattern.h
@@ -0,0 +1,18 @@
+/*
+ * Array and api for led blinking pattern
+ */
+
+#ifndef HEADERS_PATTERN
+#define HEADERS_PATTERN
+
+#define LENGTH 9
+
+typedef struct {
+    unsigned char * back;
+    unsigned char * front;
+} pattern;
+
+pattern get_left_pattern();
+pattern get_right_pattern();
+
+#endif // HEADERS_PATTERN
