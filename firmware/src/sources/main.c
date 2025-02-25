c0d9574ef22bd95c4b222310a09213dd28289990
Maksym Dybarskyi <maxim.dybarskyy@gmail.com>
2025-02-25 20:48:13 +0100

    Change main files

--- firmware/src/sources/main.c
+++ firmware/src/sources/main.c
@@ -1,17 +1,16 @@
 #include <msp430g2553.h>
+
+#include "pattern.h"
+#include "spi.h"
 #include "timer.h"
-#include "shift.h"
-#include "state.h"
 #include "switch.h"
 
 void configure() {
 	// turn off watchdog
     WDTCTL = WDTPW + WDTHOLD;
-
-	// use frequency 8 MHz
-    BCSCTL1 = CALBC1_8MHZ;
-    DCOCTL = CALDCO_8MHZ;
-
+	// use frequency 1 MHz
+    BCSCTL1 = CALBC1_1MHZ;
+    DCOCTL = CALDCO_1MHZ;
 	// all pins off
     P1OUT = 0;
     P2OUT = 0;
@@ -20,35 +19,42 @@ void configure() {
 int main(void) {
 	// configure MCU
     configure();
-
 	// configure peripherals
-    configure_shift();
+    configure_spi();
     configure_timer();
     configure_switch();
-
-	// inital state
-    state_idle();
-
 	// low power mode + enable interruptions
     _BIS_SR(LPM0_bits + GIE);
 }
 
-/* 
- * callbacks implementation
- */
+// main logic and callbacks implementation
+
+static char counter;
+static pattern current;
+
+void event_timer() {
+	if (counter >= LENGTH) {
+		counter = 0;
+	}
+	send_to_front(*current.front + counter);
+	send_to_back(*current.back + counter);
+	counter++;
+}
 
 void event_switch_left() {
-	state_left();
+	counter = 0;
+	current = get_left_pattern();
+	start_timer();
 }
 
 void event_switch_right() {
-	state_right();
+	counter = 0;
+	current = get_right_pattern();
+	start_timer();
 }
 
 void event_switch_middle() {
-	state_idle();
-}
-
-void event_timer() {
-	next_led();
+	stop_timer();
+	send_to_front(0);
+	send_to_back(0);
 }
